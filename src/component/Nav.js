c248af9184fbcdd5c45bde632bf47b67f0e417ba
miramwm <miriam_w15@yahoo.com>
2019-04-02 21:48:29 -0700

    implement react router

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,16 +1,16 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
   render() {
     return (
       <nav className="nav">
-        <a href="#"> ShopMate </a>
 
-        <a href="#"> Women </a>
-
-        <a href="#"> Men </a>
-
-        <a href="#"> Kids </a>
+        <Link to="/">ShopMate</Link>
+        <Link to="/women/">Women</Link>
+        <Link to="/men/">Men</Link>
+        <Link to="/kids/">Kids</Link>
+        <Link to="/signin/">Sign in</Link>
       </nav>
     );
   }
