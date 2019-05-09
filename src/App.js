686ddcd91d6a4fd32b03ffe2225fb38cbcb0574b
sibhat <sibhat.temelso@gmail.com>
2019-05-08 22:05:12 -0700

    nav component now update login system

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import React, {Component} from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Nav from "./component/Nav.js";
-import Signin from "./component/Signin";
-import SingUp from "./component/SignUp";
-import { Route } from "react-router-dom";
+import Nav from "./component/nav/Nav.js";
+import Auth from "./component/auth";
+import {Route} from "react-router-dom";
+
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {headline: ""};
-  }
-  componentWillMount() {
-    console.log("[app.js] componentWillMount")
-  }
-  componentDidMount() {
-    this.setState ( {headline: "Welcome to ShopMate"});
-
-    console.log("[app.js] componentDidMount")
-  }
-
-  render() {
-      console.log("[app.js] render")
-    return (
-      <div className="App">
-        <Nav />
-				<Route path="/signin" component={Signin} />
-         <Route path="/register" component={SingUp} />
-
-      </div>
-    );
-  }
+    state={
+        isLogedIn: false
+    };
+    componentDidMount() {
+        if(localStorage.token)
+            this.setState({isLogedIn: true})
+    }
+    logOut = () =>{
+        localStorage.removeItem("token");
+        this.setState({isLogedIn: false})
+    };
+    logIn =()=>{
+        this.setState({isLogedIn: true})
+    };
+    render() {
+        return (
+            <div className="App">
+                <Nav isLogedIn={this.state.isLogedIn} logOut={this.logOut}/>
+                <Route path={"/auth"} render={e => <Auth {...e} logIn={this.logIn}/>}/>
+            </div>
+        );
+    }
 }
 
 export default App;
