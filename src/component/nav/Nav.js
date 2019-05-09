94928a2dff3d02a869d54b7aabb46df7d5975dbf
sibhat <sibhat.temelso@gmail.com>
2019-05-08 22:04:31 -0700

    nav component now update login system

---
+++ src/component/nav/Nav.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="nav">
+                <Link to="/">ShopMate</Link>
+                <Link to="/women/">Women</Link>
+                <Link to="/men/">Men</Link>
+                <Link to="/kids/">Kids</Link>
+                {!this.props.isLogedIn && <Link to="/auth/login/">Sign in</Link>}
+                {!this.props.isLogedIn && <Link to="/auth/register/">SignUp</Link>}
+                {this.props.isLogedIn && <button onClick={this.props.logOut}>Logout</button>}
+            </nav>
+        );
+    }
+}
+
+export default Nav;
