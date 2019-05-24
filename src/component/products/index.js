e98744c94572c05f4f167c87a3f532fc13075a8a
sibhat <sibhat.temelso@gmail.com>
2019-05-23 23:30:11 -0700

    implement card component to use reactStrap

--- src/component/products/index.js
+++ src/component/products/index.js
@@ -7,13 +7,12 @@ import CardLink from "reactstrap/es/CardLink";
 
 class Products extends React.Component{
     state={
-        products:{
-            results: []
-        }
+        products:[]
     };
     componentDidMount() {
         axios.get('/api/products').then(result => {
-            this.setState({products: result.data})
+            this.setState({products: result.data});
+            console.log("products are", result);
         }).catch(error => {
             console.log({error});
         })
@@ -24,16 +23,16 @@ class Products extends React.Component{
 
         return (
             <CardColumns>
-            {this.state.products.results.map(card => (
-                <Card key={card.product_id}>
-                    <CardImg top width="100%" src="" alt={`${card.name.split(" ")[0]} Image Holder`} />
+            {this.state.products.map(product => (
+                <Card key={product.product_id}>
+                    <CardImg top width="100%" src="" alt={`${product.name.split(" ")[0]} Image Holder`} />
                     <CardBody>
-                        <CardLink>{card.name}</CardLink>
+                        <CardLink>{product.name}</CardLink>
                         {/*<CardSubtitle>{card.name}</CardSubtitle>*/}
-                        <CardText>{card.description}</CardText>
-                        <CardText>Price: {card.price}</CardText>
+                        <CardText>{product.description}</CardText>
+                        <CardText>Price: {product.price}</CardText>
                         <CardFooter>
-                            <Button>Buy</Button>
+                            <Button onClick={() => this.props.addItem (product)}>Buy</Button>
                         </CardFooter>
                     </CardBody>
                 </Card>
