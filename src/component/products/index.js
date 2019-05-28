80bd3fd7f68001cae389b8fd9fa1d3ca9518d478
sibhat <sibhat.temelso@gmail.com>
2019-05-28 08:35:24 -0700

    implement card component to use reactStrap

--- src/component/products/index.js
+++ src/component/products/index.js
@@ -2,34 +2,33 @@ import React from "react";
 import axios from "../../config/axios";
 import { Card, CardImg, CardText, CardBody,CardColumns, Button } from 'reactstrap';
 import CardFooter from "reactstrap/es/CardFooter";
-import CardLink from "reactstrap/es/CardLink";
-
+import "./Nav.css"
 
 class Products extends React.Component{
     state={
         products:[]
     };
     componentDidMount() {
-        axios.get('/api/products').then(result => {
-            this.setState({products: result.data});
-            console.log("products are", result);
+        axios.get('/api/products')
+            .then(result => {
+                this.setState({products: result.data.results});
         }).catch(error => {
             console.log({error});
         })
     }
 
     render() {
-        console.log(this.state.products);
-
         return (
             <CardColumns>
             {this.state.products.map(product => (
-                <Card key={product.product_id}>
-                    <CardImg top width="100%" src="" alt={`${product.name.split(" ")[0]} Image Holder`} />
+                <Card key={product.product_id} className={"card"}>
+                    <CardImg top width="100%" src={`${process.env.REACT_APP_API_URL}static/img/${product.image}`} alt={`${product.name.split(" ")[0]} Image Holder`} />
                     <CardBody>
-                        <CardLink>{product.name}</CardLink>
-                        {/*<CardSubtitle>{card.name}</CardSubtitle>*/}
-                        <CardText>{product.description}</CardText>
+                        <CardText>{product.name}</CardText>
+                        <CardText>{
+                            product.description.length > 100?
+                            product.description.slice(0, 100) + "..." : product.description
+                        }</CardText>
                         <CardText>Price: {product.price}</CardText>
                         <CardFooter>
                             <Button onClick={() => this.props.addItem (product)}>Buy</Button>
