05dc391632fe0dd87603776192701032451065e8
sibhat <sibhat.temelso@gmail.com>
2019-06-27 12:02:13 -0700

    comment some methods with what they are, or what they are doing!

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Nav from "./component/nav/Nav.js";
 import Auth from "./component/auth";
 import {Route} from "react-router-dom";
 import Products from "./component/products";
-import Checkout from "./component/strip/Checkout";
+import Steper from "./component/strip/Steper";
 
-class App extends Component {
+class App extends Component { //classed based &&& inherit from React.Component
 
     state={
         isLogedIn: false,
-        products: [],
+        cart_products: [],
         total_items: 0,
         total_amount: 0,
         added_on: Date.now(),
@@ -20,15 +20,15 @@ class App extends Component {
         let new_total_amount = (item.price * 1) + this.state.total_amount;
         let total_items = this.state.total_items+ 1;
         this.setState({
-            products: [...this.state.products, item],
+            cart_products: [...this.state.cart_products, item],
             total_items: total_items,
             total_amount: new_total_amount,
         })
     };
-    componentDidMount() {
+    componentDidMount() { //lifecycle
         if(localStorage.token)
             this.setState({isLogedIn: true})
-    }
+    } //lifecycle
     logOut = () =>{
         localStorage.removeItem("token");
         this.setState({isLogedIn: false})
@@ -36,13 +36,13 @@ class App extends Component {
     logIn =()=>{
         this.setState({isLogedIn: true})
     };
-    render() {
+    render() { // must have
         return (
             <div className="App">
                 <Nav isLogedIn={this.state.isLogedIn} logOut={this.logOut} data={this.state}/>
                 <Route path={"/auth"} render={e => <Auth {...e} logIn={this.logIn}/>}/>
                 <Route path={"/home"} exact render={e => <Products {...e} addItem={this.addItem} />} />
-                <Route path={"/checkout"} exact render={e => <Checkout {...e}  />} />
+                <Route path={"/checkout"} exact render={e => <Steper {...e}  />} />
             </div>
         );
     }
